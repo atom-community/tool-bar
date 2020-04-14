8c08c4b12b8e19d85becc8e9f851bb79f9e608f4
aminya <aminyahyaabadi74@gmail.com>
2020-04-14 17:28:13 -0500

    Remove travis, circleci, appveyor, remark script

--- scripts/remark-lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f ./node_modules/.bin/remark ]; then
-    echo "Linting MarkDown..."
-    ./node_modules/.bin/remark -f .
-    rc=$?; if [ $rc -ne 0 ]; then exit $rc; fi
-fi
-
-exit
